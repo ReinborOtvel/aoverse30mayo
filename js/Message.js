12b26001102d52d2fc4a76b26edbce0d4b7b46ab
ReinborOtvel <johansebastianreinborotvel@gmail.com>
2022-06-04 07:01:45 -0500

    asdf

--- js/Message.js
+++
@@ -1,9 +0,0 @@
-export default class Message {
-  constructor(text) {
-    this.text = text;
-  }
-  draw() {
-    window.Main.Draw.interfaceEmpty.draw();
-    window.Main.Text.word(this.text, 30, 30);
-  }
-}
--- js/start.js
+++
@@ -1,3 +0,0 @@
-import Main from "./Main.js";
-window.Main = new Main();
-window.Main.start();
