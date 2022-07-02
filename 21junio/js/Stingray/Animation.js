88eef1ab7b83582afad02f044163605308866faf
ProyectosReinbor <108497659+ProyectosReinbor@users.noreply.github.com>
2022-07-02 14:01:03 -0300

    1 julio

--- 21junio/js/Stingray/Animation.js
+++
@@ -1,6 +0,0 @@
-export default class AnimationStingray {
-  constructor() {
-    this.image = new image
-  }
-  update() { }
-}
--- 30junio/js/start.js
+++
@@ -1,3 +0,0 @@
-import Main from "./Main.js";
-window.main = new Main();
-window.main.start();
