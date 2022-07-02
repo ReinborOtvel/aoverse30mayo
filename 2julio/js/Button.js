fa089a0e9729c39d2b2d2bea4d30f465cce62531
ProyectosReinbor <108497659+ProyectosReinbor@users.noreply.github.com>
2022-07-02 15:03:29 -0500

    ajksdjfj

---
+++ 2julio/js/Button.js
@@ -0,0 +1,24 @@
+import Image from "./Image.js";
+export default class Button {
+  constructor(text, transform, clickCall) {
+    this.text = text;
+    this.clickCall = clickCall;
+    this.image = new Image("./img/interface/button.png", {
+      x: 0, y: 0, width: 369, height: 26
+    }, transform);
+    this.xInitial = this.image.transform.x;
+    this.xEnd = this.xInitial + this.image.transform.width;
+    this.yInitial = this.image.transform.y;
+    this.yEnd = this.yInitial + this.image.transform.height;
+    this.size = (this.image.transform.height / 100) * 80;
+    this.yWord = this.yInitial - ((this.image.transform.height - this.size) / 2);
+  }
+  update() {
+    if (window.main.click.verify(this.xInitial, this.yInitial, this.xEnd, this.yEnd) === true) {
+      this.clickCall();
+    }
+    this.image.update();
+    let xInitial = this.xInitial + this.size;
+    window.main.draw.word(this.text, xInitial, this.yWord, this.size);
+  }
+}
