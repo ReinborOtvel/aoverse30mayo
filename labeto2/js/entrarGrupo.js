10b902292245778ae0c2302c537717e7dd34560e
proyectosreinbor <proyectosreinbor@gmail.com>
2022-07-24 17:31:57 -0500

    asdf

--- labeto2/js/entrarGrupo.js
+++
@@ -1,9 +0,0 @@
-export default class EntrarGrupo {
-    constructor(main) {
-        this.main = main;
-        this.interfazVacia = this.main.loadImage("assets/interfazVacia.png");
-    }
-    draw() {
-        this.main.image(this.interfazVacia, 0, 0, 640, 360);
-    }
-}
--- labeto2/juego/mapa.js
+++
@@ -1,9 +0,0 @@
-export default class Mapa {
-    constructor(graf, personaje) {
-        this.graf = graf;
-        this.personaje = personaje;
-    }
-    preload() {
-
-    }
-}
