0709ff458a9b2166977b305a969bd4fc8d37f02a
ProyectosReinbor <108497659+ProyectosReinbor@users.noreply.github.com>
2022-08-26 17:09:21 -0500

    Update EnterGroup.js

--- labeto1/js/EnterGroup.js
+++ labeto1/js/EnterGroup.js
@@ -1,36 +1,33 @@
 "use strict";
+import verifyClick from "./verifyClick.js";
 export default class {
     setup() {
         this.interfaceEmpty = engine.loadImage("./Graficos/Interfaces/empty.png");
         this.account = data.account.toUpperCase();
         for (let leader in data.information.groups) {
             if (data.information.groups[leader].length < 5) {
-                data.info.statistics[this.account] = data.statistics;
                 this.leader = leader;
-                data.info.leaders[this.account] = leader;
-                data.info.groups[leader].push(this.account);
-                data.info.groups[this.account] = [];
+                data.information.statistics[this.account] = data.statistics;
+                data.information.leaders[this.account] = this.leader;
+                data.information.groups[this.account] = [];
+                data.information.groups[this.leader].push(this.account);
                 break;
             }
         }
+        this.player = new Player({ x: 300, y: 50, width: 150, height: 150, }, data.statistics);
         if (this.leader == undefined) {
             alert("not groups");
-            return;
+        } else {
+            this.leaderPlayer = new Player({ x: 450, y: 50, width: 150, height: 150, },
+                data.information.statistics[this.leader]);
         }
-        this.player = new Player({
-            x: 300, y: 50, width: 150, height: 150,
-        }, data.statistics);
-        this.leaderPlayer = new Player({
-            x: 450, y: 50, width: 150, height: 150,
-        }, data.info.statistics[this.leader]);
     }
     click() {
         console.log(data.click);
-        if (Verify(57, 287, 282, 330)) {
-            data.database.set(JSON.stringify(data.info)).then(() => {
-                setTimeout(() => {
-                    location.reload();
-                }, 5000);
+        if (verifyClick(57, 287, 282, 330)) {
+            data.database.set(JSON.stringify(data.information)).then(() => {
+                setTimeout(() => location.reload(), 5000);
+                alert("wait five seconds");
             }).catch(error => {
                 console.error(error);
                 alert("error, set database");
