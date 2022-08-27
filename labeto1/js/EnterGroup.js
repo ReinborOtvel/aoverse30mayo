0ffa36dada5aa3168431f595f93797ab03f39e26
ProyectosReinbor <108497659+ProyectosReinbor@users.noreply.github.com>
2022-08-27 10:58:33 -0500

    Update EnterGroup.js

--- labeto1/js/EnterGroup.js
+++ labeto1/js/EnterGroup.js
@@ -1,5 +1,7 @@
 "use strict";
 import verifyClick from "./verifyClick.js";
+import Player from "./Player.js";
+import textBackground from "./textBackground.js";
 export default class {
     setup() {
         this.interfaceEmpty = engine.loadImage("./Graficos/Interfaces/empty.png");
@@ -10,15 +12,16 @@ export default class {
                 data.information.statistics[this.account] = data.statistics;
                 data.information.leaders[this.account] = this.leader;
                 data.information.groups[this.account] = [];
-                data.information.groups[this.leader].push(this.account);
+                for (let i = 0; i < 5; i++)
+                    data.information.groups[this.leader].push(this.account);
                 break;
             }
         }
-        this.player = new Player({ x: 300, y: 50, width: 150, height: 150, }, data.statistics);
+        this.player = new Player({ x: 80, y: 235, width: 90, height: 90, }, data.statistics);
         if (this.leader == undefined) {
             alert("not groups");
         } else {
-            this.leaderPlayer = new Player({ x: 450, y: 50, width: 150, height: 150, },
+            this.leaderPlayer = new Player({ x: 360, y: 30, width: 150, height: 150, },
                 data.information.statistics[this.leader]);
         }
     }
@@ -35,21 +38,22 @@ export default class {
         }
     }
     draw() {
-        if (this.leader == undefined) {
-            return;
-        }
         engine.image(this.interfaceEmpty, 0, 0, 640, 360);
-        engine.text("team", 50, 70);
-        let leader = this.leader.slice(0, 5);
-        engine.text(`leader ${leader}`, 50, 100);
-        let y = 100;
-        for (let i = 0; i < data.info.groups[this.leader].length; i++) {
-            let member = data.info.groups[this.leader][i];
-            y += 30;
-            engine.text(`member${i + 1} ${member.slice(0, 5)}`, 50, y);
-        }
-        TextBackground(50, 280, "  join the group");
+        engine.text("team", 50, 55);
+        textBackground(50, 250, "  join the group");
         this.player.draw();
-        this.leaderPlayer.draw();
+        if (this.leader != undefined) {
+            for (let i = 0; i < data.information.groups[this.leader].length; i++) {
+                let member = data.information.groups[this.leader][i];
+                let y = 25 * i;
+                y += 80;
+                engine.text(`member${i + 1} ${member.slice(0, 5)}`, 50, y);
+            }
+            this.leaderPlayer.draw();
+            let statisticsLeader = this.leaderPlayer.statistics;
+            engine.text(`leader ${this.leader.slice(0, 5)}`, 50, 210);
+            engine.text(`name ${statisticsLeader.name}`, 50, 235);
+            engine.text(`strength ${statisticsLeader.strength}`, 50, 260);
+        }
     }
 }
