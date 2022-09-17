3a26727b458d8d130bf4175fe3397f54bd1da284
ProyectosReinbor <108497659+ProyectosReinbor@users.noreply.github.com>
2022-09-16 20:28:55 -0500

    sep16 2028

--- labeto1/js/Map.js
+++ labeto1/js/Map.js
@@ -2,7 +2,7 @@
 export default class {
   constructor(fullLoad) {
     this.fullLoad = fullLoad;
-    this.level = [
+    this.soil = [
       [579, 579, 579, 579, 579, 579, 579, 579, 579, 579],
       [579, 579, 579, 579, 579, 579, 579, 579, 579, 579],
       [579, 579, 579, 579, 579, 579, 579, 579, 579, 579],
@@ -14,19 +14,26 @@ export default class {
       [579, 579, 579, 579, 579, 579, 579, 579, 579, 579],
       [579, 579, 579, 579, 579, 579, 579, 579, 579, 579],
     ];
+    this.tree = [
+      [-1, 1, -1, 1, -1],
+      [-1, 1, -1, 1, -1],
+      [-1, 1, -1, 1, -1],
+      [-1, 1, -1, 1, -1],
+      [-1, 1, -1, 1, -1],
+    ];
     this.imgs();
   }
   imgs() {
     this.imgs = {};
-    engine.loadImage("./graphics/nature/579.png", img => {
-      this.imgs[579] = img;
-      this.fullLoad();
-    });
+    this.imgs[1] = engine.loadImage("./graphics/nature/1.png");
+    this.imgs[579] = engine.loadImage("./graphics/nature/579.png");
+    this.fullLoad();
   }
-  draw() {
+  drawSoil() {
     for (let h = 0; h < 10; h++) {
       for (let v = 0; v < 10; v++) {
-        let index = this.level[h][v];
+        let index = this.soil[h][v];
+        if (index == -1) continue;
         let width = 10;
         let height = 10;
         let x = width * h;
@@ -34,5 +41,22 @@ export default class {
         utils.image(this.imgs[index], x, y, width, height);
       }
     }
+  }
+  drawTree() {
+    for (let h = 0; h < 5; h++) {
+      for (let v = 0; v < 5; v++) {
+        let index = this.tree[h][v];
+        if (index == -1) continue;
+        let width = 20;
+        let height = 20;
+        let x = width * h;
+        let y = height * v;
+        utils.image(this.imgs[index], x, y, width - 10, height);
+      }
+    }
+  }
+  draw() {
+    this.drawSoil();
+    this.drawTree();
   }
 }
