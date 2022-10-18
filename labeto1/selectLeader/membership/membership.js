8a8a19e32721e7666c8c9e1e56223a3a0128eed9
ProyectosReinbor <108497659+ProyectosReinbor@users.noreply.github.com>
2022-10-17 19:08:19 -0500

    Update membership.js

--- labeto1/selectLeader/membership/membership.js
+++ labeto1/selectLeader/membership/membership.js
@@ -1,4 +1,25 @@
 "use strict";
 export default function () {
-  window.membership =
-}
+  window.membership = {};
+  window.membership.membership = [];
+  window.membership.countMembers = accounts => {
+    for (let account of accounts) {
+      let owner = account.owner.toUpperCase();
+      window.membership.membership[owner] = 0;
+    }
+    for (let account of accounts) {
+      let leader = account.leader.toUpperCase();
+      window.membership.membership[leader]++;
+    }
+  };
+  window.membership.availableLeaders = accounts => {
+    let user = metamask.account.toUpperCase();
+    for (let account of accounts) {
+      let owner = account.owner.toUpperCase();
+      if (owner == user) continue;
+      if (membership[owner] > 4) continue;
+      leaders.push(account.owner);
+    }
+    RandomLeader();
+  };
+};
